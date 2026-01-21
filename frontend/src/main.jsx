ce86d67c4f1b631e5cc4c5de6346c30433a9f675
Mateo Gurrea <mategurrea@gmail.com>
2026-01-21 16:31:45 -0300

    estructura front

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
 
-import React from "react"
-import ReactDOM from "react-dom/client"
-
-function App() {
-  return <h1>Evolution SaaS Frontend</h1>
-}
-
-ReactDOM.createRoot(document.getElementById("root")).render(<App />)
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <h1 className='animate-fadeIn text-amber-900 '>Hello world</h1>
+  </StrictMode>,
+)
